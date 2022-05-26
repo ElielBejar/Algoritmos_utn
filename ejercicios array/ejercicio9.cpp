0c8a22fbc4cfdf847ad8832567526ca16bee5779
Eliel Bejar <elielbejar12@gmail.com>
2022-05-25 22:20:33 -0300

    funcion obtenerDatos finalizada

--- ejercicios array/ejercicio9.cpp
+++ ejercicios array/ejercicio9.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#define ALUMNOS 10
 
 using namespace std;
 
 //obtiene dni y notas
-void obtenerDatos(){
+void obtenerDatos(int alumnos[10][2]){
 
+  for(int i = 0; i<ALUMNOS; i++){
+     cout<<"Ingrese dni del alumno: ";
+     cin>>alumnos[i][0];
+     cout<<"Ingrese nota del alumno: ";
+     cin>>alumnos[i][1];
+  }
 }
 
 //ordena las notas de mayor a menor
