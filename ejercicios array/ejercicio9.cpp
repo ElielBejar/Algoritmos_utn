27b6f2f6d9ae22d6e5e3573dc4e7359d335af1f7
Eliel Bejar <elielbejar12@gmail.com>
2022-05-25 22:08:14 -0300

    estructura ejercicio 9

--- ejercicios array/ejercicio9.cpp
+++ ejercicios array/ejercicio9.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 
 using namespace std;
+
+//obtiene dni y notas
+void obtenerDatos(){
+
+}
+
+//ordena las notas de mayor a menor
+void ordenarNotas(){
+
+}
+
+//muestra el contenido del array
+void mostrarDatos(){
+
+}
+
+int main(){
+
+  return 0;
+}
